631101e155ee380b53130cd735c100105cfc4c59
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-12-26 18:06:33 +0900

    12.26 NAVER news crawler APP base

---
+++ android/app/src/main/kotlin/com/example/crawling_with_beautifuldart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crawling_with_beautifuldart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:beautiful_soup_dart/beautiful_soup.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
